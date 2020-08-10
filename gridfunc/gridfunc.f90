7a1f93dcc3ca57e79c0f09d1d9657bb35eeabf54
spneville <sneville.sneville@gmail.com>
2020-08-10 16:36:36 -0400

    Start of work on a program to extract the grid representations of the diabatic state representation of adiabatic state density matrix elements.

---
+++ gridfunc/gridfunc.f90
@@ -0,0 +1,16 @@
+!######################################################################
+! gridfunc: A program for the calculation of various functions on
+!           direct product grids:
+!           (1) Projectors onto adiabatic states
+!           (2) Adiabatic electronic state density matrix operators
+!######################################################################
+program gridfunc
+
+  use constants
+  
+  implicit none
+
+
+  
+end program gridfunc
+  
